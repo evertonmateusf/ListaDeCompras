28bfd66ab96cdfd12c1a75db05a68f13f54a21e7
Everton Mateus Fernandes <everton.mfernandes@gmail.com>
2020-04-13 19:22:38 -0300

    removidas telas não utilizadas até o momento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { FinalizarCompraPageModule } from './../pages/finalizar-compra/finalizar-compra.module';
-import { AdicionarProdutoPageModule } from './../pages/adicionar-produto/adicionar-produto.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { EditarCompraPage } from '../pages/editar-compra/editar-compra';
-import { AdicionarProdutoPage } from '../pages/adicionar-produto/adicionar-produto';
 import { FinalizarCompraPage } from '../pages/finalizar-compra/finalizar-compra';
 import { StorageService } from '../services/storage.service';
 import { ProdutoService } from '../services/produto.service';
@@ -20,22 +17,18 @@ import { ProdutoService } from '../services/produto.service';
   declarations: [
     MyApp,
     HomePage,
-    ListPage,
     EditarCompraPage,
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    AdicionarProdutoPageModule,
     FinalizarCompraPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage,
     EditarCompraPage,
-    AdicionarProdutoPage,
     FinalizarCompraPage
   ],
   providers: [

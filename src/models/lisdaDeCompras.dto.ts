18572fa32a24b9bcd3ae39e3b3ff025d30de3592
Everton Mateus Fernandes <everton.mfernandes@gmail.com>
2020-04-13 19:17:59 -0300

    unificada adicao de produto com a lista de compra

--- src/models/lisdaDeCompras.dto.ts
+++ src/models/lisdaDeCompras.dto.ts
@@ -1,7 +1,9 @@
 import { ProdutoDaListaDTO } from './produtoDaLista.dto';
 export class ListaDeComprasDTO {
+    codigo: string;
     descricao: string;
     produtos: ProdutoDaListaDTO[];
     valor: number;
     mercado: string;
+    situacao: string;
 }
--- src/pages/editar-compra/editar-compra.ts
+++ src/pages/editar-compra/editar-compra.ts
@@ -3,6 +3,8 @@ import { ProdutoDTO } from './../../models/produto.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, reorderArray, AlertController } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
+import { AddProdutoDTO } from '../../models/addProduto.dto';
+import { ProdutoService } from '../../services/produto.service';
 
 @IonicPage()
 @Component({
@@ -11,12 +13,17 @@ import { StorageService } from '../../services/storage.service';
 })
 export class EditarCompraPage {
   itens: ProdutoDaListaDTO[];
+  itensFiltrados: AddProdutoDTO[];
   reordenar: boolean;
+  searchQuery: string;
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public alertCtrl: AlertController,
-    public storage: StorageService) {
+    public storage: StorageService,
+    public produtoService: ProdutoService) {
     this.reordenar = false;
+    this.searchQuery = "";
+    this.itensFiltrados = [];
     this.itens = [
       { descricao: 'Suco', quantidade: 2, observacao: '', comprado: false },
       { descricao: 'Arroz', quantidade: 1, observacao: 'Trazer Prato Fino', comprado: false },
@@ -30,20 +37,22 @@ export class EditarCompraPage {
       { descricao: 'Molho de tomate', quantidade: 1, observacao: 'Pomarola', comprado: true }
     ]
     this.salvarLista();
-    
+
   }
-  ionViewDidEnter(){
+  ionViewDidEnter() {
     let lista = this.storage.getLista()
-    if (lista){
+    if (lista) {
       this.itens = lista.produtos;
     }
   }
-  salvarLista(){
-    const lista = {
+  salvarLista() {
+    let lista = {
+      "codigo": "1",
       "descricao": "Minha lista",
-      "produtos":this.itens,
-      "valor":0,
-      "mercado":"Assaí"
+      "produtos": this.itens,
+      "valor": 0,
+      "mercado": "Assaí",
+      "situacao": "EM ANDAMENTO"
     }
     this.storage.setLista(lista);
   }
@@ -51,18 +60,24 @@ export class EditarCompraPage {
   marcarComoComprado(item: ProdutoDaListaDTO) {
     item.comprado = true;
     this.salvarLista()
-    // this.itensComprados.unshift(item);
-    // this.itens = this.itens.filter(function (elemento) { return elemento.descricao != item.descricao; });
   }
 
   voltarParaAListaComprar(item: ProdutoDaListaDTO) {
     item.comprado = false;
     this.salvarLista()
-    // this.itens.push(item);
-    // this.itensComprados = this.itensComprados.filter(function (elemento) { return elemento.descricao != item.descricao; });
   }
   adicionarItem() {
-    this.navCtrl.push('AdicionarProdutoPage', {});
+    let produtos = this.itensFiltrados.filter(item => item.isChecked).map(item => {
+      return {
+        "descricao": item.descricao,
+        "quantidade": 1,
+        "observacao": "",
+        "comprado": false
+      }
+    })
+    this.itens = produtos.concat(this.itens)
+    this.salvarLista()
+    this.searchQuery = "";
   }
   excluirItem(item: ProdutoDTO) {
     let alert = this.alertCtrl.create({
@@ -96,5 +111,38 @@ export class EditarCompraPage {
     return itens.filter(function (el) {
       return el.comprado === comprado;
     })
+  }
+  iniciaFiltro() {
+    let produtosOrdenados = this.produtoService.getProdutos().sort(function (a, b) {
+      if (a.descricao >= b.descricao) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }).map(item => { return { descricao: item.descricao, isChecked: false } })
+    let produtosNaLista = this.storage.getLista().produtos
+    if (produtosNaLista) {
+      produtosOrdenados = produtosOrdenados.filter(item => {
+        return produtosNaLista.filter(itemNaLista => {
+          return itemNaLista.descricao === item.descricao
+        }).length === 0
+      })
+    }
+    this.itensFiltrados = produtosOrdenados
+  }
+  filterList(evt) {
+    this.itensFiltrados = [];
+    if (!this.searchQuery) {
+      return;
+    }
+    this.iniciaFiltro();
+    this.itensFiltrados = this.itensFiltrados.filter(currentGoal => {
+      if (currentGoal.descricao && this.searchQuery) {
+        if (currentGoal.descricao.toLowerCase().indexOf(this.searchQuery.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      }
+    }).map(item => { return { descricao: item.descricao, isChecked: false } });
   }
 }
